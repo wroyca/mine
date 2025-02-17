2c96d6317bc0c166c38671cd715b0c48fc984f98
William Roy <wroy@proton.me>
2025-02-17 12:35:56 -0500

    Initial commit

---
+++ libmine/libmine/mine.cxx
@@ -0,0 +1,18 @@
+#include <libmine/mine.hxx>
+
+#include <ostream>
+#include <stdexcept>
+
+using namespace std;
+
+namespace mine
+{
+  void
+  say_hello (ostream& o, const string& n)
+  {
+    if (n.empty ())
+      throw invalid_argument ("empty name");
+
+    o << "Hello, " << n << '!' << endl;
+  }
+}
