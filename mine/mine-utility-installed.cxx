1700d85adcb145a15926806405a745c331c2e737
William Roy <wroy@proton.me>
2026-04-03 14:44:26 -0400

    Expose build2 installation paths to runtime

--- mine/mine-utility-installed.cxx
+++ mine/mine-utility-installed.cxx
@@ -2,11 +2,15 @@
 
 #include <filesystem>
 
-using namespace std::filesystem;
+using namespace std;
+using namespace filesystem;
 
 namespace mine
 {
   bool build_installed = true;
+
+  // Base installation directories.
+  //
 
 #ifdef MINE_INSTALL_ROOT
   path build_install_root (MINE_INSTALL_ROOT);
@@ -20,9 +24,204 @@ namespace mine
   path build_install_root_relative;
 #endif
 
+#ifdef MINE_INSTALL_DATA_ROOT
+  path build_install_data_root (MINE_INSTALL_DATA_ROOT);
+#else
+  path build_install_data_root;
+#endif
+
+#ifdef MINE_INSTALL_DATA_ROOT_RELATIVE
+  path build_install_data_root_relative (MINE_INSTALL_DATA_ROOT_RELATIVE);
+#else
+  path build_install_data_root_relative;
+#endif
+
+#ifdef MINE_INSTALL_EXEC_ROOT
+  path build_install_exec_root (MINE_INSTALL_EXEC_ROOT);
+#else
+  path build_install_exec_root;
+#endif
+
+#ifdef MINE_INSTALL_EXEC_ROOT_RELATIVE
+  path build_install_exec_root_relative (MINE_INSTALL_EXEC_ROOT_RELATIVE);
+#else
+  path build_install_exec_root_relative;
+#endif
+
+  // Executables and libraries.
+  //
+
+#ifdef MINE_INSTALL_BIN
+  path build_install_bin (MINE_INSTALL_BIN);
+#else
+  path build_install_bin;
+#endif
+
+#ifdef MINE_INSTALL_BIN_RELATIVE
+  path build_install_bin_relative (MINE_INSTALL_BIN_RELATIVE);
+#else
+  path build_install_bin_relative;
+#endif
+
+#ifdef MINE_INSTALL_SBIN
+  path build_install_sbin (MINE_INSTALL_SBIN);
+#else
+  path build_install_sbin;
+#endif
+
+#ifdef MINE_INSTALL_SBIN_RELATIVE
+  path build_install_sbin_relative (MINE_INSTALL_SBIN_RELATIVE);
+#else
+  path build_install_sbin_relative;
+#endif
+
+#ifdef MINE_INSTALL_LIB
+  path build_install_lib (MINE_INSTALL_LIB);
+#else
+  path build_install_lib;
+#endif
+
+#ifdef MINE_INSTALL_LIB_RELATIVE
+  path build_install_lib_relative (MINE_INSTALL_LIB_RELATIVE);
+#else
+  path build_install_lib_relative;
+#endif
+
+#ifdef MINE_INSTALL_LIBEXEC
+  path build_install_libexec (MINE_INSTALL_LIBEXEC);
+#else
+  path build_install_libexec;
+#endif
+
+#ifdef MINE_INSTALL_LIBEXEC_RELATIVE
+  path build_install_libexec_relative (MINE_INSTALL_LIBEXEC_RELATIVE);
+#else
+  path build_install_libexec_relative;
+#endif
+
+#ifdef MINE_INSTALL_PKGCONFIG
+  path build_install_pkgconfig (MINE_INSTALL_PKGCONFIG);
+#else
+  path build_install_pkgconfig;
+#endif
+
+#ifdef MINE_INSTALL_PKGCONFIG_RELATIVE
+  path build_install_pkgconfig_relative (MINE_INSTALL_PKGCONFIG_RELATIVE);
+#else
+  path build_install_pkgconfig_relative;
+#endif
+
+  // Architecture-independent data and configuration.
+  //
+
+#ifdef MINE_INSTALL_ETC
+  path build_install_etc (MINE_INSTALL_ETC);
+#else
+  path build_install_etc;
+#endif
+
+#ifdef MINE_INSTALL_ETC_RELATIVE
+  path build_install_etc_relative (MINE_INSTALL_ETC_RELATIVE);
+#else
+  path build_install_etc_relative;
+#endif
+
+#ifdef MINE_INSTALL_INCLUDE
+  path build_install_include (MINE_INSTALL_INCLUDE);
+#else
+  path build_install_include;
+#endif
+
+#ifdef MINE_INSTALL_INCLUDE_RELATIVE
+  path build_install_include_relative (MINE_INSTALL_INCLUDE_RELATIVE);
+#else
+  path build_install_include_relative;
+#endif
+
+#ifdef MINE_INSTALL_INCLUDE_ARCH
+  path build_install_include_arch (MINE_INSTALL_INCLUDE_ARCH);
+#else
+  path build_install_include_arch;
+#endif
+
+#ifdef MINE_INSTALL_INCLUDE_ARCH_RELATIVE
+  path build_install_include_arch_relative (MINE_INSTALL_INCLUDE_ARCH_RELATIVE);
+#else
+  path build_install_include_arch_relative;
+#endif
+
+#ifdef MINE_INSTALL_SHARE
+  path build_install_share (MINE_INSTALL_SHARE);
+#else
+  path build_install_share;
+#endif
+
+#ifdef MINE_INSTALL_SHARE_RELATIVE
+  path build_install_share_relative (MINE_INSTALL_SHARE_RELATIVE);
+#else
+  path build_install_share_relative;
+#endif
+
 #ifdef MINE_INSTALL_DATA
   path build_install_data (MINE_INSTALL_DATA);
 #else
   path build_install_data;
+#endif
+
+#ifdef MINE_INSTALL_DATA_RELATIVE
+  path build_install_data_relative (MINE_INSTALL_DATA_RELATIVE);
+#else
+  path build_install_data_relative;
+#endif
+
+#ifdef MINE_INSTALL_BUILDFILE
+  path build_install_buildfile (MINE_INSTALL_BUILDFILE);
+#else
+  path build_install_buildfile;
+#endif
+
+#ifdef MINE_INSTALL_BUILDFILE_RELATIVE
+  path build_install_buildfile_relative (MINE_INSTALL_BUILDFILE_RELATIVE);
+#else
+  path build_install_buildfile_relative;
+#endif
+
+  // Documentation and man pages.
+  //
+
+#ifdef MINE_INSTALL_DOC
+  path build_install_doc (MINE_INSTALL_DOC);
+#else
+  path build_install_doc;
+#endif
+
+#ifdef MINE_INSTALL_DOC_RELATIVE
+  path build_install_doc_relative (MINE_INSTALL_DOC_RELATIVE);
+#else
+  path build_install_doc_relative;
+#endif
+
+#ifdef MINE_INSTALL_LEGAL
+  path build_install_legal (MINE_INSTALL_LEGAL);
+#else
+  path build_install_legal;
+#endif
+
+#ifdef MINE_INSTALL_LEGAL_RELATIVE
+  path build_install_legal_relative (MINE_INSTALL_LEGAL_RELATIVE);
+#else
+  path build_install_legal_relative;
+#endif
+
+#ifdef MINE_INSTALL_MAN
+  path build_install_man (MINE_INSTALL_MAN);
+#else
+  path build_install_man;
+#endif
+
+#ifdef MINE_INSTALL_MAN_RELATIVE
+  path build_install_man_relative (MINE_INSTALL_MAN_RELATIVE);
+#else
+  path build_install_man_relative;
 #endif
 }
--- mine/mine-utility.hxx
+++ mine/mine-utility.hxx
@@ -5,24 +5,76 @@
 
 namespace mine
 {
-  extern bool build_installed;
-
-  extern std::filesystem::path build_install_lib;       // $install.lib
-  extern std::filesystem::path build_install_buildfile; // $install.buildfile
-  extern std::filesystem::path build_install_data;      // $install.data
-  extern std::filesystem::path build_install_root;      // $install.root
-
-  extern std::filesystem::path build_install_root_relative;
-
-  // Resolves the standard user configuration directory.
-  //   Linux/macOS: $XDG_CONFIG_HOME/mine  (or ~/.config/mine)
-  //   Windows:     %APPDATA%\mine
-  //
   std::optional<std::filesystem::path>
   get_user_config_dir ();
 
-  // Helper to directly resolve the main initialization file.
-  //
   std::optional<std::filesystem::path>
   get_user_config_file ();
+
+  //
+
+  extern bool build_installed;
+
+  // Base installation directories.
+  //
+
+  extern std::filesystem::path build_install_root;
+  extern std::filesystem::path build_install_root_relative;
+
+  extern std::filesystem::path build_install_data_root;
+  extern std::filesystem::path build_install_data_root_relative;
+
+  extern std::filesystem::path build_install_exec_root;
+  extern std::filesystem::path build_install_exec_root_relative;
+
+  // Executables and libraries.
+  //
+
+  extern std::filesystem::path build_install_bin;
+  extern std::filesystem::path build_install_bin_relative;
+
+  extern std::filesystem::path build_install_sbin;
+  extern std::filesystem::path build_install_sbin_relative;
+
+  extern std::filesystem::path build_install_lib;
+  extern std::filesystem::path build_install_lib_relative;
+
+  extern std::filesystem::path build_install_libexec;
+  extern std::filesystem::path build_install_libexec_relative;
+
+  extern std::filesystem::path build_install_pkgconfig;
+  extern std::filesystem::path build_install_pkgconfig_relative;
+
+  // Architecture-independent data and configuration.
+  //
+
+  extern std::filesystem::path build_install_etc;
+  extern std::filesystem::path build_install_etc_relative;
+
+  extern std::filesystem::path build_install_include;
+  extern std::filesystem::path build_install_include_relative;
+
+  extern std::filesystem::path build_install_include_arch;
+  extern std::filesystem::path build_install_include_arch_relative;
+
+  extern std::filesystem::path build_install_share;
+  extern std::filesystem::path build_install_share_relative;
+
+  extern std::filesystem::path build_install_data;
+  extern std::filesystem::path build_install_data_relative;
+
+  extern std::filesystem::path build_install_buildfile;
+  extern std::filesystem::path build_install_buildfile_relative;
+
+  // Documentation and man pages.
+  //
+
+  extern std::filesystem::path build_install_doc;
+  extern std::filesystem::path build_install_doc_relative;
+
+  extern std::filesystem::path build_install_legal;
+  extern std::filesystem::path build_install_legal_relative;
+
+  extern std::filesystem::path build_install_man;
+  extern std::filesystem::path build_install_man_relative;
 }
