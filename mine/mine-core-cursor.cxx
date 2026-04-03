cb1568939807077c2a3b62a89edfa0a541a7d9c0
William Roy <wroy@proton.me>
2026-04-03 16:04:19 -0400

    Separate cursor interface from implementation

--- mine/mine-core-cursor.cxx
+++ mine/mine-core-cursor.cxx
@@ -1,1 +1,193 @@
 #include <mine/mine-core-cursor.hxx>
+
+#include <algorithm>
+
+using namespace std;
+
+namespace mine
+{
+  cursor cursor::
+  move_to (cursor_position p) const noexcept
+  {
+    cursor c (*this);
+    c.p_ = p;
+    return c;
+  }
+
+  cursor cursor::
+  move_left (const text_buffer& b) const noexcept
+  {
+    // If there's still room on the current line, we just step back by one
+    // cluster. Simple enough.
+    //
+    if (p_.column.value > 0)
+      return move_to (cursor_position (p_.line,
+                                       column_number (p_.column.value - 1)));
+
+    // We've hit the left wall (column 0). Let's see if we can wrap around to
+    // the end of the previous line.
+    //
+    if (p_.line.value > 0)
+    {
+      line_number l (p_.line.value - 1);
+
+      // We want to snap exactly to the end of that preceding line.
+      //
+      size_t n (b.line_length (l));
+
+      return move_to (cursor_position (l, column_number (n)));
+    }
+
+    // We are at the very beginning of the buffer (0,0), so there's nowhere left
+    // to go. Just stay put.
+    //
+    return *this;
+  }
+
+  cursor cursor::
+  move_right (const text_buffer& b) const noexcept
+  {
+    size_t n (b.line_length (p_.line));
+
+    // As long as we haven't crossed the line boundary physically, just advance
+    // the column.
+    //
+    if (p_.column.value < n)
+      return move_to (cursor_position (p_.line,
+                                       column_number (p_.column.value + 1)));
+
+    // We are at the end of the current line. Attempt to wrap around to the very
+    // beginning of the next line.
+    //
+    if (p_.line.value + 1 < b.line_count ())
+      return move_to (cursor_position (line_number (p_.line.value + 1),
+                                       column_number (0)));
+
+    return *this;
+  }
+
+  cursor cursor::
+  move_up (const text_buffer& b) const noexcept
+  {
+    if (p_.line.value > 0)
+    {
+      line_number l (p_.line.value - 1);
+      size_t n (b.line_length (l));
+
+      // Vertical movement can be tricky since lines vary in length. Here we use
+      // a hard clamp: if we move from a long line up to a shorter one, we
+      // simply clamp the column to the end of the new short line.
+      //
+      column_number c (min (p_.column.value, n));
+
+      return move_to (cursor_position (l, c));
+    }
+
+    return *this;
+  }
+
+  cursor cursor::
+  move_down (const text_buffer& b) const noexcept
+  {
+    if (p_.line.value + 1 < b.line_count ())
+    {
+      line_number l (p_.line.value + 1);
+      size_t n (b.line_length (l));
+
+      // Similar to moving up, we clamp the column if the destination line
+      // happens to be shorter than our current column position.
+      //
+      column_number c (min (p_.column.value, n));
+
+      return move_to (cursor_position (l, c));
+    }
+
+    return *this;
+  }
+
+  cursor cursor::
+  move_line_start () const noexcept
+  {
+    // Jump straight to the first column.
+    //
+    return move_to (cursor_position (p_.line, column_number (0)));
+  }
+
+  cursor cursor::
+  move_line_end (const text_buffer& b) const noexcept
+  {
+    // Find how many clusters are in this line and snap to the end.
+    //
+    size_t n (b.line_length (p_.line));
+    return move_to (cursor_position (p_.line, column_number (n)));
+  }
+
+  cursor cursor::
+  move_buffer_start () const noexcept
+  {
+    return move_to (cursor_position (line_number (0), column_number (0)));
+  }
+
+  cursor cursor::
+  move_buffer_end (const text_buffer& b) const noexcept
+  {
+    // An empty buffer is a special case. If there are no lines, we just point
+    // to (0,0).
+    //
+    if (b.line_count () == 0)
+      return move_buffer_start ();
+
+    line_number l (b.line_count () - 1);
+    size_t n (b.line_length (l));
+
+    return move_to (cursor_position (l, column_number (n)));
+  }
+
+  cursor cursor::
+  clamp_to_buffer (const text_buffer& b) const noexcept
+  {
+    cursor c (*this);
+
+    // First, handle the pathological case where the buffer has been completely
+    // cleared. We must reset to (0,0) and drag the mark along if it exists.
+    //
+    if (b.line_count () == 0)
+    {
+      c.p_ = cursor_position (line_number (0), column_number (0));
+
+      if (c.m_)
+        c.m_ = c.p_;
+
+      return c;
+    }
+
+    // We might be totally out of bounds if lines were deleted by an external
+    // mutation (like undo/redo or a background save), so we first clamp the
+    // line index.
+    //
+    line_number l (min (p_.line.value, b.line_count () - 1));
+
+    // Now that we have a valid line, we clamp the column so that it doesn't
+    // exceed the line's new length.
+    //
+    size_t n (b.line_length (l));
+    column_number cn (min (p_.column.value, n));
+
+    c.p_ = cursor_position (l, cn);
+
+    // Don't forget the selection mark. If the buffer changed under us, the mark
+    // might also be pointing into the void, so we repeat the clamping process
+    // for it.
+    //
+    if (c.m_)
+    {
+      line_number ml (min (c.m_->line.value, b.line_count () - 1));
+      size_t mn (b.line_length (ml));
+      column_number mc (min (c.m_->column.value, mn));
+
+      c.m_ = cursor_position (ml, mc);
+    }
+
+    return c;
+  }
+}
--- mine/mine-core-cursor.hxx
+++ mine/mine-core-cursor.hxx
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <algorithm>
+#include <compare>
+#include <optional>
 
 #include <mine/mine-types.hxx>
 #include <mine/mine-contract.hxx>
@@ -16,7 +17,7 @@ namespace mine
   // If the user presses "Right" while standing on a flag emoji, we increment
   // the column by 1, which might correspond to jumping 10+ bytes in the buffer.
   //
-  // In other words, we relies on the buffer to define what "column N" actually
+  // In other words, we rely on the buffer to define what "column N" actually
   // means in memory.
   //
   class cursor
@@ -51,42 +52,27 @@ namespace mine
       return p_.column;
     }
 
-    // Selection mark.
+    // Selection mark
     //
 
-    // Set the selection mark at the current point.
-    //
-    // This anchors one end of the selection. Note that as the point moves
-    // subsequently, the region between the mark and the point forms the
-    // active selection highlight.
-    //
     void
     set_mark () noexcept
     {
       m_ = p_;
     }
 
-    // Clear the active selection.
-    //
-    // We typically trigger this when the user presses Escape or performs
-    // an action that cancels the highlight.
-    //
     void
     clear_mark () noexcept
     {
       m_.reset ();
     }
 
-    // Return true if we have an active selection.
-    //
     bool
     has_mark () const noexcept
     {
       return m_.has_value ();
     }
 
-    // Get the current mark position.
-    //
     cursor_position
     mark () const noexcept
     {
@@ -99,207 +85,46 @@ namespace mine
     //
 
     cursor
-    move_to (cursor_position p) const noexcept
-    {
-      cursor c (*this);
-      c.p_ = p;
-      return c;
-    }
+    move_to (cursor_position p) const noexcept;
 
-    // Move backwards (Left Arrow).
-    //
-    // Standard wrapping logic: if we hit the left wall (column 0), we try to
-    // wrap up to the end of the previous line.
-    //
     cursor
-    move_left (const text_buffer& b) const noexcept
-    {
-      // If we have room on the current line, just step back.
-      //
-      if (p_.column.value > 0)
-      {
-        return move_to (cursor_position (p_.line,
-                                         column_number (p_.column.value - 1)));
-      }
+    move_left (const text_buffer& b) const noexcept;
 
-      // We are at column 0. Try to wrap to the previous line.
-      //
-      if (p_.line.value > 0)
-      {
-        line_number l (p_.line.value - 1);
-
-        // Snap to the end of that line.
-        //
-        std::size_t n (b.line_length (l));
-
-        return move_to (cursor_position (l, column_number (n)));
-      }
-
-      // We are at (0,0). Nowhere to go.
-      //
-      return *this;
-    }
-
-    // Move forwards (Right Arrow).
-    //
-    // Wrapping logic: if we hit the end of the line (past the last grapheme),
-    // we wrap to the start of the next line.
-    //
     cursor
-    move_right (const text_buffer& b) const noexcept
-    {
-      std::size_t n (b.line_length (p_.line));
+    move_right (const text_buffer& b) const noexcept;
 
-      // Check if we are physically within the line boundaries.
-      //
-      if (p_.column.value < n)
-      {
-        return move_to (cursor_position (p_.line,
-                                         column_number (p_.column.value + 1)));
-      }
-
-      // We are at the end. Try to wrap to the start of the next line.
-      //
-      if (p_.line.value + 1 < b.line_count ())
-      {
-        return move_to (cursor_position (line_number (p_.line.value + 1),
-                                         column_number (0)));
-      }
-
-      // End of buffer.
-      //
-      return *this;
-    }
-
-    // Move Up.
-    //
-    // Vertical movement is slightly tricky because lines have different
-    // lengths.
-    //
-    // The current behavior is a "hard clamp". That is, if we move from a long
-    // line to a short line, we clamp the cursor to the end of the short line.
-    //
     cursor
-    move_up (const text_buffer& b) const noexcept
-    {
-      if (p_.line.value > 0)
-      {
-        line_number l (p_.line.value - 1);
-        std::size_t n (b.line_length (l));
+    move_up (const text_buffer& b) const noexcept;
 
-        // Clamp column to the new line's length.
-        //
-        column_number c (std::min (p_.column.value, n));
-
-        return move_to (cursor_position (l, c));
-      }
-
-      return *this;
-    }
-
-    // Move Down.
-    //
-    // Same clamping logic as move_up.
-    //
     cursor
-    move_down (const text_buffer& b) const noexcept
-    {
-      if (p_.line.value + 1 < b.line_count ())
-      {
-        line_number l (p_.line.value + 1);
-        std::size_t n (b.line_length (l));
-
-        column_number c (std::min (p_.column.value, n));
-
-        return move_to (cursor_position (l, c));
-      }
-
-      return *this;
-    }
+    move_down (const text_buffer& b) const noexcept;
 
     // Semantic Jumps
     //
 
     cursor
-    move_line_start () const noexcept
-    {
-      return move_to (cursor_position (p_.line, column_number (0)));
-    }
+    move_line_start () const noexcept;
 
     cursor
-    move_line_end (const text_buffer& b) const noexcept
-    {
-      std::size_t n (b.line_length (p_.line));
-      return move_to (cursor_position (p_.line, column_number (n)));
-    }
+    move_line_end (const text_buffer& b) const noexcept;
 
     cursor
-    move_buffer_start () const noexcept
-    {
-      return move_to (cursor_position (line_number (0), column_number (0)));
-    }
+    move_buffer_start () const noexcept;
 
     cursor
-    move_buffer_end (const text_buffer& b) const noexcept
-    {
-      if (b.line_count () == 0)
-        return move_buffer_start ();
+    move_buffer_end (const text_buffer& b) const noexcept;
 
-      line_number l (b.line_count () - 1);
-      std::size_t n (b.line_length (l));
-
-      return move_to (cursor_position (l, column_number (n)));
-    }
-
-    // Validation.
+    // Validation
     //
-    // This is useful after an external buffer mutation (like a undo/redo or
-    // a background save) where the cursor might end up pointing to a line
-    // that no longer exists or a column that is now out of bounds.
-    //
+
     cursor
-    clamp_to_buffer (const text_buffer& b) const noexcept
-    {
-      cursor c (*this);
-
-      if (b.line_count () == 0)
-      {
-        c.p_ = cursor_position (line_number (0), column_number (0));
-        if (c.m_)
-          c.m_ = c.p_;
-        return c;
-      }
-
-      // 1. Clamp line index.
-      //
-      line_number l (std::min (p_.line.value, b.line_count () - 1));
-
-      // 2. Clamp column index.
-      //
-      std::size_t n (b.line_length (l));
-      column_number cn (std::min (p_.column.value, n));
-
-      c.p_ = cursor_position (l, cn);
-
-      if (c.m_)
-      {
-        line_number ml (std::min (c.m_->line.value, b.line_count () - 1));
-        std::size_t mn (b.line_length (ml));
-        column_number mc (std::min (c.m_->column.value, mn));
-        c.m_ = cursor_position (ml, mc);
-      }
-
-      return c;
-    }
+    clamp_to_buffer (const text_buffer& b) const noexcept;
 
     auto
     operator<=> (const cursor&) const noexcept = default;
 
   private:
     cursor_position p_;
-
-    // The selection anchor. If this has a value, a selection is active.
-    //
     std::optional<cursor_position> m_;
   };
 }
